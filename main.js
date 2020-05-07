4acc7c79061c9388acd1e5fb82d08c02eba24ac8
Frederic Chen <fredericchen@Frederics-MacBook-Pro.local>
2020-05-07 14:21:07 -0400

    FINAL

--- main.js
+++ main.js
@@ -5,7 +5,10 @@ $(document).ready(function() {
     $("#aboutme").css("display", "block");
     $("#ex").css("display", "block");
     $("#face").css("display", "block");
-      $(".container").css('filter','brightness(50%)');
+    $("#contact").css("display", "block");
+    $("#twitter").css("display", "block");
+    $("#insta").css("display", "block");
+    $(".container").css('filter', 'brightness(50%)');
   });
 
   $("#ex").click(function() {
@@ -13,9 +16,12 @@ $(document).ready(function() {
     $("#face").css("display", "none");
     $("#ex").css("display", "none");
     $("#games").css("display", "none");
-        $("#youtubedesc").css("display", "none");
+    $("#youtubedesc").css("display", "none");
     $("#skills").css("display", "none");
-        $(".container").css('filter','brightness(100%)');
+    $(".container").css('filter', 'brightness(100%)');
+    $("#contact").css("display", "none");
+    $("#twitter").css("display", "none");
+    $("#insta").css("display", "none");
   });
 
 
@@ -23,100 +29,83 @@ $(document).ready(function() {
     $("#games").css("display", "block");
     $("#face").css("display", "block");
     $("#ex").css("display", "block");
-        $(".container").css('filter','brightness(50%)');
+    $(".container").css('filter', 'brightness(50%)');
   });
 
   $("#plaque").click(function() {
     $("#youtubedesc").css("display", "block");
     $("#face").css("display", "block");
     $("#ex").css("display", "block");
-        $(".container").css('filter','brightness(50%)');
+    $(".container").css('filter', 'brightness(50%)');
   });
 
   $("#camera").click(function() {
     $("#skills").css("display", "block");
     $("#face").css("display", "block");
     $("#ex").css("display", "block");
-        $(".container").css('filter','brightness(50%)');
+    $(".container").css('filter', 'brightness(50%)');
   });
 
   $("#photo2").click(function() {
-    $(".container").css('filter','brightness(50%)');
+    $(".container").css('filter', 'brightness(50%)');
     $("#next").css("display", "block");
     $("#prev").css("display", "block");
     $("#exSlide1").css("display", "block");
-    $("#nature1").css("display", "block");
-    showSlides();
+    showSlides(1, 0);
   });
 
   $("#exSlide1").click(function() {
     $("#next").css("display", "none");
     $("#prev").css("display", "none");
-      $("#exSlide1").css("display", "none");
-      $(".container").css('filter','brightness(100%)');
-  $(".mySlides").css("display", "none");
+    $("#next2").css("display", "none");
+    $("#prev2").css("display", "none");
+    $("#exSlide1").css("display", "none");
+    $(".container").css('filter', 'brightness(100%)');
+    $(".mySlides").css("display", "none");
+    $(".mySlides2").css("display", "none");
+
   });
 
   $("#photo1").click(function() {
-    $(".container").css('filter','brightness(50%)');
+    $(".container").css('filter', 'brightness(50%)');
     $("#next2").css("display", "block");
     $("#prev2").css("display", "block");
     $("#exSlide1").css("display", "block");
-    $("#portrait1").css("display", "block");
-
+    showSlides(1, 1);
   });
-
 });
 
 
+
+
+
 // SOURCE: https://www.w3schools.com/howto/howto_js_slideshow.asp
-var slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex = [1, 1];
+var slideId = ["mySlides", "mySlides2"]
+showSlides(1, 0);
+showSlides(1, 1);
 
 // Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
 }
 
 // Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function currentSlide(n, no) {
+  showSlides(slideIndex[no] = n, no);
 }
 
-function showSlides(n) {
+function showSlides(n, no) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {
+    slideIndex[no] = 1
   }
-
-  slides[slideIndex-1].style.display = "block";
-}
-
-// SECOND SLIDESHOW
-var slideInde = 1;
-showSlide(slideInde);
-
-// Next/previous controls
-function plusSlide(n) {
-  showSlide(slideInde += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideInde = n);
-}
-
-function showSlide(n) {
-  var z;
-  var slide = document.getElementsByClassName("mySlide");
-  if (n > slide.length) {slideInde = 1}
-  if (n < 1) {slideInde = slide.length}
-  for (i = 0; i < slide.length; i++) {
-      slide[z].style.display = "none";
+  if (n < 1) {
+    slideIndex[no] = x.length
   }
-
-  slide[slideInde-1].style.display = "block";
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex[no] - 1].style.display = "block";
 }
