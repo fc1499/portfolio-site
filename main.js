add9776ebe5d1dcf00436b9291e8350a70fece44
Frederic Chen <fredericchen@Frederics-MacBook-Pro.local>
2020-05-06 22:20:56 -0400

    push

--- main.js
+++ main.js
@@ -1,131 +1,38 @@
 $(document).ready(function() {
-// CITE: found in playful-website-examples
-  $("#first").click(function() {
-    $("#first_img").css("display", "block");
-    $("#text1").css("display", "block");
-        $("#close").css("display", "block");
-    $("#morning").css("display", "none");
-        $("#night").css("display", "none");
+
+  $("#myself").click(function() {
+    $("#aboutme").css("display", "block");
+    $("#ex").css("display", "block");
+    $("#face").css("display", "block");
   });
 
-  $("#first_img").click(function() {
-            $("#close").css("display", "none");
-    $("#first_img").css("display", "none");
-      $("#line").css("display", "block");
-    $("#text1").css("display", "none");
-    $("#second").css("display", "block");
-    $("#morning").css("display", "block");
-    $("#night").css("display", "block");
-  });
-
-  $("#second").click(function() {
-    $("#second_img").css("display", "block");
-            $("#close").css("display", "block");
-    $("#text2").css("display", "block");
-    $("#morning").css("display", "none");
-        $("#night").css("display", "none");
-  });
-
-  $("#second_img").click(function() {
-            $("#close").css("display", "none");
-    $("#second_img").css("display", "none");
-    $("#line2").css("display", "block");
-    $("#text2").css("display", "none");
-    $("#third").css("display", "block");
-    $("#morning").css("display", "block");
-    $("#night").css("display", "block");
-  });
-
-  $("#third").click(function() {
-    $("#third_img").css("display", "block");
-            $("#close").css("display", "block");
-    $("#text3").css("display", "block");
-    $("#morning").css("display", "none");
-        $("#night").css("display", "none");
-  });
-
-  $("#third_img").click(function() {
-            $("#close").css("display", "none");
-    $("#third_img").css("display", "none");
-    $("#text3").css("display", "none");
-    $("#fourth").css("display", "block");
-    $("#morning").css("display", "block");
-    $("#night").css("display", "block");
-  });
-
-  $("#fourth").click(function() {
-    $("#fourth_img").css("display", "block");
-            $("#close").css("display", "block");
-    $("#text6").css("display", "block");
-    $("#morning").css("display", "none");
-        $("#night").css("display", "none");
-  });
-
-  $("#fourth_img").click(function() {
-            $("#close").css("display", "none");
-
-      $("#line4").css("display", "block");
-    $("#fourth_img").css("display", "none");
-    $("#text6").css("display", "none");
-    $("#fifth").css("display", "block");
-    $("#morning").css("display", "block");
-    $("#night").css("display", "block");
-  });
-
-  $("#fifth").click(function() {
-            $("#close").css("display", "block");
-    $("#fifth_img").css("display", "block");
-    $("#text5").css("display", "block");
-    $("#morning").css("display", "none");
-        $("#night").css("display", "none");
-  });
-
-  $("#fifth_img").click(function() {
-            $("#close").css("display", "none");
-    $("#line3").css("display", "block");
-    $("#fifth_img").css("display", "none");
-    $("#text5").css("display", "none");
-    $("#sixth").css("display", "block");
-    $("#morning").css("display", "block");
-    $("#night").css("display", "block");
+  $("#ex").click(function() {
+    $("#aboutme").css("display", "none");
+    $("#face").css("display", "none");
+    $("#ex").css("display", "none");
+    $("#games").css("display", "none");
+        $("#youtubedesc").css("display", "none");
+    $("#skills").css("display", "none");
   });
 
 
-    $("#sixth").click(function() {
-              $("#close").css("display", "block");
-      $("#sixth_img").css("display", "block");
-      $("#text4").css("display", "block");
-      $("#morning").css("display", "none");
-          $("#night").css("display", "none");
-    });
-
-    $("#sixth_img").click(function() {
-              $("#close").css("display", "none");
-      $("#sixth_img").css("display", "none");
-      $("#text4").css("display", "none");
-      $("#sixth").css("display", "block");
-      $("#morning").css("display", "block");
-      $("#night").css("display", "block");
-    });
-
-  $("#morning").click(function() {
-    $("body").css('animation','gradient2 8s infinite');
-        $("body").css('background-color','#721b65');
+  $("#rose").click(function() {
+    $("#games").css("display", "block");
+    $("#face").css("display", "block");
+    $("#ex").css("display", "block");
   });
 
-  $("#night").click(function() {
-    $("body").css('animation','gradient 8s infinite');
-        $("body").css('background-color','#003494');
+  $("#plaque").click(function() {
+    $("#youtubedesc").css("display", "block");
+    $("#face").css("display", "block");
+    $("#ex").css("display", "block");
   });
 
-// https://stackoverflow.com/questions/31565045/toggle-background-image-html
-  $("#rain").click(function() {
-    $("body").toggleClass('BgClass');
+  $("#camera").click(function() {
+    $("#skills").css("display", "block");
+    $("#face").css("display", "block");
+    $("#ex").css("display", "block");
   });
 
-// https://stackoverflow.com/questions/31565045/toggle-background-image-html
-  $("#hearts").click(function() {
-  $("body").toggleClass('BgClass2');
-  });
 
 });
